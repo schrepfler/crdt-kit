067f85c15fa74209e29a506b7f30e4e723a2fc40
Srdan Srepfler <schrepfler@gmail.com>
2016-05-16 23:45:41 +0200

    Moving to case classes

---
+++ src/test/scala/net/sigmalab/crdt/GCounterImplTest.scala
@@ -0,0 +1,26 @@
+package net.sigmalab.crdt
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by schrepfler on 16/05/2016.
+  */
+class GCounterImplTest extends FunSuite {
+
+  test("testMerge") {
+
+  }
+
+  test("testValue") {
+
+  }
+
+  test("testPayload") {
+
+  }
+
+  test("testEquals") {
+
+  }
+
+}
